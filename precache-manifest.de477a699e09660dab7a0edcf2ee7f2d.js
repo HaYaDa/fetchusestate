e1ddc19e4ea4c83628e3888aab811345bc7b0e61
H-Sun <hasanyasindagkus@hotmail.com>
2019-11-05 12:21:39 +0100

    Updates

---
+++ precache-manifest.de477a699e09660dab7a0edcf2ee7f2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42bcf88d8cf7d68b865b32deae6ff16",
+    "url": "/fetchusestate/index.html"
+  },
+  {
+    "revision": "54bc7e7d762df14fb5c9",
+    "url": "/fetchusestate/static/css/main.5dfffe4a.chunk.css"
+  },
+  {
+    "revision": "7b14ae0dc170b68f35cc",
+    "url": "/fetchusestate/static/js/2.6f0fa24a.chunk.js"
+  },
+  {
+    "revision": "54bc7e7d762df14fb5c9",
+    "url": "/fetchusestate/static/js/main.1bac11e9.chunk.js"
+  },
+  {
+    "revision": "542219685220aff81cf8",
+    "url": "/fetchusestate/static/js/runtime-main.1eb96622.js"
+  }
+]);
